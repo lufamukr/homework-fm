940bdf3df536f63a3af036b265d98a7563cd4100
lufamukr <lufamukr@gmail.com>
2024-03-01 21:17:55 +0100

    hw4 monday

--- src/HW/4/HW4.tsx
+++ src/HW/4/HW4.tsx
@@ -22,29 +22,33 @@ export const HW4 = () => {
     setCurrentText('');
   };
 
+  const handler = (val:string) => {
+    setCurrentText(val);
+  }
+
   return (
     <div id={'hw04'}>
-      {/*{currentText ? (*/}
-      {/*  <h1 id={'hw04-text'}>{currentText}</h1>*/}
-      {/*) : (*/}
-      {/*  <h1 id={'hw04-default-text'}>Здесь появится новое дело</h1>*/}
-      {/*)}*/}
+      {currentText ? (
+       <h1 id={'hw04-text'}>{currentText}</h1>
+      ) : (
+       <h1 id={'hw04-default-text'}>Здесь появится новое дело</h1>
+      )}
 
-      {/*<Input НЕ ХВАТАЕТ АРГУМЕНТОВ />*/}
+      <Input setCurrentText={handler} currentText={currentText}/>
 
-      {/*<Button НЕ ХВАТАЕТ АРГУМЕНТОВ />*/}
+      <Button callBack={handleSave} name='Save'/>
 
-      {/*<h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>*/}
+      <h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>
 
-      {/*<ol id={'hw04-tasks'}>*/}
-      {/*  {texts.map((el, index) => {*/}
-      {/*    return (*/}
-      {/*      <li key={index} id={`hw04-task-${index}`} className={ЕСЛИ ЧЕТН ? S.chetNechet : ''}>*/}
-      {/*        {el}*/}
-      {/*      </li>*/}
-      {/*    );*/}
-      {/*  })}*/}
-      {/*</ol>*/}
+      <ol id={'hw04-tasks'}>
+       {texts.map((el, index) => {
+         return (
+           <li key={index} id={`hw04-task-${index}`} className={index % 2 === 0 ? s.chetNechet : ''}>
+             {el}
+           </li>
+         );
+       })}
+      </ol>
     </div>
   );
 };
